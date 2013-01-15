ad8027f4eb4acbb39cb187480facb33d5b5d5f64
Vasily Gotovko <vasily.gotovko@gmail.com>
2013-01-15 13:36:08 +0300

    Hide 'Log time' on page. Check permissions. Refactoring

--- assets/javascripts/redmine_time_limit.js
+++ assets/javascripts/redmine_time_limit.js
@@ -1,18 +1,42 @@
-function showHideLogTime() {
-  var protected_statuses = $('#protected_statuses')[0].getAttribute('data');
-  var new_status_id = $('#issue_status_id')[0].value
-  var log_time = $('#time_entry_hours').parents('fieldset.tabular:first')[0]
-  if ($.inArray(new_status_id, protected_statuses) > 0 && $.inArray(initial_status_id, protected_statuses) > 0) {
-    log_time.style.display = 'none';
-  } else {
-    log_time.style.display = '';
+function getTimeLimitData(){
+  return JSON.parse($('#protected_statuses')[0].getAttribute('data'));
+}
+
+function showHideLogTimeFieldset() {
+  var data = getTimeLimitData();
+  var new_status_id = $('#issue_status_id')[0].value;
+  var log_time_fieldset = $('#time_entry_hours').parents('fieldset.tabular:first')[0];
+  var hide = (($.inArray(new_status_id, data.protected_status_ids) > 0) && ($.inArray(data.initial_status_id, data.protected_status_ids) > 0))
+  timeLimitShowHideElement(log_time_fieldset, hide)
+}
+
+function hideShowLogTimeIconIfProtected() {
+  var data = getTimeLimitData();
+  var log_time_icon = $('a.icon-time-add');
+  var hide = ($.inArray(data.initial_status_id, data.protected_status_ids) > 0)
+
+  for (var i = 0; i < log_time_icon.length; i++) {
+    timeLimitShowHideElement(log_time_icon[i], hide);
   }
 }
 
-$(document).ready(function() {
+function timeLimitShowHideElement(element, hide) {
+  if (hide) {
+    element.style.display = 'none';
+  } else {
+    element.style.display = '';
+  }
+}
+
+hideShowLogTimeIconIfProtected();
+
+$(document.body).ready(function() {
+  // call this function twice: in loading process and after document ready
+  // first calling to avoid displaying/blinking "Log time" at top
+  // seconnd one to hide "Log time" at bottom
+  hideShowLogTimeIconIfProtected();
   if ($('#issue_status_id')[0]) {
-    initial_status_id = $('#issue_status_id')[0].value;
-    showHideLogTime();
-    $('#issue_status_id')[0].onchange = showHideLogTime;
+    showHideLogTimeFieldset();
+    $('#issue_status_id')[0].onchange = showHideLogTimeFieldset;
   }
 })
--- lib/redmine_time_limit/hooks.rb
+++ lib/redmine_time_limit/hooks.rb
@@ -2,10 +2,14 @@ module RedmineTimeLimit
   class Hooks < Redmine::Hook::ViewListener
 
     def view_issues_show_details_bottom(context={})
-      js = javascript_include_tag('redmine_time_limit.js', :plugin => 'redmine_time_limit')
+      issue = context[:issue]
+      unless TimeEntry.have_permissions?(User.current, issue.project)
+        js = javascript_include_tag('redmine_time_limit.js', :plugin => 'redmine_time_limit')
 
-      @settings = Setting.plugin_redmine_time_limit
-      "#{js} <div id='protected_statuses' data='#{@settings[:status_ids].to_json}'></div>"
+        @settings = Setting.plugin_redmine_time_limit
+        data = { :protected_status_ids => @settings[:status_ids], :initial_status_id => issue.status_id.to_s }
+        "<div id='protected_statuses' data='#{data.to_json}'></div> #{js}"
+      end
     end
 
   end
