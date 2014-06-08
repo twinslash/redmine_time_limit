e39a2685f3c3c4a869a957735cc06da5f985dfb6
Serge Ahranovich <c.xander@gmail.com>
2014-06-08 20:23:22 +0300

    try to fix time limit

--- lib/redmine_time_limit/time_limit_issue_patch.rb
+++ lib/redmine_time_limit/time_limit_issue_patch.rb
@@ -9,14 +9,16 @@ module TimeLimitIssuePatch
 
       attr_accessor :time_limit_allowed_ip
 
-      alias_method_chain :save_issue_with_child_records, :time_limit
+      # alias_method_chain :save_issue_with_child_records, :time_limit
+      alias_method_chain :update, :time_limit
 
     end
   end
 
   module InstanceMethods
 
-    def save_issue_with_child_records_with_time_limit(params, existing_time_entry=nil)
+    # def save_issue_with_child_records_with_time_limit(params, existing_time_entry=nil)
+    def update_with_time_limit(params, existing_time_entry=nil)
       existing_time_entry ||= TimeEntry.new
       existing_time_entry.time_limit_allowed_ip = time_limit_allowed_ip
 
